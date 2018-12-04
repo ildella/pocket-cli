5da7778cd20d247f7deee4f94cc8681d7761fd3b
Daniele Dellafiore <ildella@gmail.com>
2018-12-04 14:36:40 +0000

    trying to have at least one smoke test on index, failing for latest node generators compativility with jest...

---
+++ test/smoke.test.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+// const pocket = require('../src/pocket/pocket-read')
+// const cli = require('../src/cli/cli')
+const index = require('../src')
+
+test('read', async () => {
+  // const output = await pocket.read()
+  // expect(output).toHaveLength(5)
+})
+
+test('start', async () => {
+  // const output = await pocket.read()
+  // expect(output).toHaveLength(5)
+})
