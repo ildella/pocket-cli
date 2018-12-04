da37394f897d51038392173a28e2e45644d24b9d
Daniele Dellafiore <ildella@gmail.com>
2018-12-04 14:36:48 +0000

    added npm audit badge

--- test/pocket-addon.test.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-const pocket = require('../src/pocket/pocket-read')
-
-test('read', async () => {
-  // const output = await pocket.read()
-  // expect(output).toHaveLength(5)
-})
