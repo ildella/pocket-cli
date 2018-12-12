eceef901494f5578d8568179617b1e2daf31cf18
Daniele Dellafiore <ildella@gmail.com>
2018-12-11 22:10:53 -0300

    moved the smoke test into a separate folder until I find a nice wawy to have a test token for the Pocket API for the CI

--- jest.ci.js
+++ jest.ci.js
@@ -2,7 +2,7 @@ module.exports = {
   verbose: false,
   notify: false,
   testEnvironment: 'node',
-  testMatch: ['**/?(*.)(spec|test).js?(x)'],
+  testMatch: ['**/test/**/?(*.)+(spec|test).js?(x)'],
   collectCoverage: true,
   coverageReporters: ['json', 'lcov', 'text', 'clover', 'html', 'json-summary']
 }
