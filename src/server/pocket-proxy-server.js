a6f060d53474817c62ba98348589216b4d8cf236
Daniele Dellafiore <ildella@gmail.com>
2018-12-14 12:38:51 -0300

    cleanup old code, now looks like a almost-decent proxy

--- src/server/pocket-proxy-server.js
+++ src/server/pocket-proxy-server.js
@@ -23,48 +23,44 @@ app.get('/', (req, res) => {
 const consumerKey = secrets.POCKET || new Error('Pocket consumer_key undefined')
 const pocketApi = require('./pocket-http')
 
-app.post('/oauth/request', async (req, res) => {
-  const payload = Object.assign({consumer_key: consumerKey}, req.body)
-  // console.log(payload)
-  const response = await pocketApi.post('/oauth/request', payload)
-  res.json(response.data)
-})
-
-app.post('/oauth/authorize', async (req, res) => {
-  const payload = Object.assign({consumer_key: consumerKey}, req.body)
-  // console.log(payload)
-  const response = await pocketApi.post('/oauth/authorize', payload)
-  res.json(response.data)
-})
-
-app.post('/send', async (req, res) => {
-  const payload = Object.assign({consumer_key: consumerKey}, req.body)
-  // console.log(payload)
-  const response = await pocketApi.post('/send', payload)
-  res.json(response.data)
-})
-
-app.post('/get', async (req, res) => {
-  const payload = Object.assign({consumer_key: consumerKey}, req.body)
-  // console.log(payload)
-  const response = await pocketApi.post('/get', payload)
-  tracer.info(response.status, typeof response.status)
-  tracer.info(response.data)
-  // tracer.info(response.headers)
-  // tracer.info(response.config)
-  // tracer.info(response)
-  res.status(response.status).json(response.data)
-  // res.set(response.headers)
-  // res.json(response.data)
-})
-
-// const httpProxy = require('http-proxy')
-// const apiProxy = httpProxy.createProxyServer()
-// const serverOne = 'https://getpocket.com/v3'
-
-// app.all('/*', (req, res) => {
-//   tracer.info('redirecting to Pocket')
-//   apiProxy.web(req, res, {target: serverOne})
+// app.post('/oauth/request', async (req, res) => {
+//   const payload = Object.assign({consumer_key: consumerKey}, req.body)
+//   const response = await pocketApi.post('/oauth/request', payload)
+//   res.json(response.data)
 // })
+
+// app.post('/oauth/authorize', async (req, res) => {
+//   const payload = Object.assign({consumer_key: consumerKey}, req.body)
+//   const response = await pocketApi.post('/oauth/authorize', payload)
+//   res.json(response.data)
+// })
+
+// app.post('/send', async (req, res) => {
+//   const payload = Object.assign({consumer_key: consumerKey}, req.body)
+//   const response = await pocketApi.post('/send', payload)
+//   res.json(response.data)
+// })
+
+// app.post('/get', async (req, res) => {
+//   const payload = Object.assign({consumer_key: consumerKey}, req.body)
+//   // console.log(payload)
+//   const response = await pocketApi.post('/get', payload)
+//   tracer.info(response.status, typeof response.status)
+//   tracer.info(response.data)
+//   // tracer.info(response.headers)
+//   // tracer.info(response.config)
+//   // tracer.info(response)
+//   res.status(response.status).json(response.data)
+//   // res.set(response.headers)
+//   // res.json(response.data)
+// })
+
+app.all('/*', async (req, res) => {
+  const payload = Object.assign({consumer_key: consumerKey}, req.body)
+  const response = await pocketApi.post('/get', payload)
+  // tracer.info(response.status, typeof response.status)
+  // tracer.info(response.data)
+  res.status(response.status).json(response.data)
+})
 
 module.exports = webtask.fromExpress(app)
