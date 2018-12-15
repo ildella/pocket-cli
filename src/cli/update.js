c5c8f8ffa209311e872bc31571c6e132fa38e7bb
Daniele Dellafiore <ildella@gmail.com>
2018-12-15 11:27:43 -0300

    support printing release notes file + command to actually run the update

--- src/cli/update.js
+++ src/cli/update.js
@@ -1,3 +1,10 @@
+const fs = require('fs')
+const readline = require('readline')
+const inputStream = fs.createReadStream('RELEASES.md')
+const rl = readline.createInterface({input: inputStream})
+const output = []
+rl.on('line', line => { output.push(line) })
+
 const commands = require('./interpreter').commands
 const update = require('../npm-update')
 
@@ -5,12 +12,28 @@ commands['update'] = {
   name: 'update',
   aliases: ['u'],
   description: 'Pocket CLI update',
-  parse: () => {
-    return {
-      name: 'version',
-      execute: async () => {
-        const check = await update.check()
-        return {lines: [`Installed: ${check.actual}. Last: ${check.last}. Need to update: ${check.need}`]}
+  parse: options => {
+    const subcommand = options && options.length > 0 ? options[0] : null
+    switch (subcommand) {
+    case 'whatsnew':
+      return {
+        name: 'update-release',
+        execute: async () => {
+          return {lines: output}
+        }
+      }
+    default:
+      return {
+        name: 'update',
+        execute: async () => {
+          const check = await update.check()
+          const output = [`Running: ${check.actual}. Last: ${check.last}. Need to update: ${check.need}`]
+          if (check.need) {
+            output.push('To update, "quit" the app then run:')
+            output.push('npm i -g pocket-cli')
+          }
+          return {lines: output}
+        }
       }
     }
   }
