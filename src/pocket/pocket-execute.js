cbd69c78b3bcabca23167b7bd72c0c5fc39b7a2e
Daniele Dellafiore <ildella@gmail.com>
2018-12-26 16:03:42 -0300

    fear driven programming removed

--- src/pocket/pocket-execute.js
+++ src/pocket/pocket-execute.js
@@ -26,8 +26,7 @@ const pocket = {
       auth: auth.get()
     })
     const response = await client.retrieve(search)
-    const data = response.data
-    const articles = Object.values(data.list)
+    const articles = Object.values(response.data.list)
     const parsedArticles = articles.map(article => {
       const id = article.resolved_id || article.item_id || '<No ID>'
       const authors = Object.values(article.authors || {}).map(author => author.name)
