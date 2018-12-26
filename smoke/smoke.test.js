fabdfedd2c78bfa1d030c74e30e09e3066244b4f
Daniele Dellafiore <ildella@gmail.com>
2018-12-26 16:02:43 -0300

    test was not aligned..

--- smoke/smoke.test.js
+++ smoke/smoke.test.js
@@ -1,30 +1,30 @@
-const interpreter = require('../src/cli/interpreter')
+const processor = require('../src/cli/process-input')
 require('../src/pocket/pocket-commands')
 require('../src/cli/version')
 require('../src/cli/quit')
 require('../src/cli/help')
 
 test('read from pocket API', async () => {
-  const output = await interpreter.processInput('bitcoin')
+  const output = await processor.processInput('bitcoin')
   expect(output.lines).toHaveLength(12)
-  await interpreter.processInput('n')
+  await processor.processInput('n')
 })
 
 test('modify with pocket API - archive/readd', async () => {
-  await interpreter.processInput('bitcoin')
-  const output = await interpreter.processInput('a 2')
+  await processor.processInput('bitcoin')
+  const output = await processor.processInput('a 2')
   expect(output.lines).toEqual(['success'])
 })
 
 test('multiple archive', async () => {
-  await interpreter.processInput('bitcoin')
-  const output = await interpreter.processInput('a 2 3')
+  await processor.processInput('bitcoin')
+  const output = await processor.processInput('a 2 3')
   expect(output.lines).toEqual(['success,success'])
 })
 
 test('archive and readd', async () => {
-  const list = await interpreter.processInput('bitcoin')
+  const list = await processor.processInput('bitcoin')
   // expect(list.lines[2]).toContain('2.  (A)')
-  const output = await interpreter.processInput('2')
+  const output = await processor.processInput('2')
   expect(output.lines).toContain('4. Re-add')
 })
