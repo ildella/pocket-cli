595ccf6f865d56fbc792f935be037b46bf76c5ae
Daniele Dellafiore <ildella@gmail.com>
2019-01-05 16:33:48 -0300

    added tags in the content output, and cut the title a little more.

--- src/pocket/articles-formatter.js
+++ src/pocket/articles-formatter.js
@@ -7,15 +7,16 @@ const renderArticle = (entry, index) => {
   const margin1 = ' '.repeat(3 - indexOutput.length)
   const url = entry.shortUrl.replace('http', 'https').replace('https://', '')
   const urlOutput = `[${url.substring(0, url.indexOf('/'))}]`
-  const titleOutput = entry.title.substring(0, maxColumns - urlOutput.length - 25)
+  const titleOutput = entry.title.substring(0, (maxColumns / 2) - 10)
   const margin2 = ' '.repeat(5)
   const excerptOutput = entry.excerpt.substring(0, maxColumns - 15)
   const timeAdded = DateTime.fromMillis(Number(entry.time_added) * 1000).toISODate()
   const contentType = entry.isArticle ? 'Article' : 'Web Page'
-  const details = `(${contentType}, ${entry.word_count} words)`
   const archived = entry.isArchived ? '(A) ' : ''
   const favorite = entry.isFavorite ? '(*) ' : ''
-  return ` ${cyan(indexOutput)}.${margin1}${archived}${favorite}${green(titleOutput)} ${yellow(urlOutput)} ${cyan(timeAdded)} ${magenta(details)}\n${margin2}${excerptOutput}\n`
+  const details = `(${contentType}, ${entry.word_count} words)`
+  const tags = Object.keys(entry.tags || []).join(', ')
+  return ` ${cyan(indexOutput)}.${margin1}${archived}${favorite}${green(titleOutput)} ${yellow(urlOutput)} ${cyan(timeAdded)} ${magenta(details)} [${tags}]\n${margin2}${excerptOutput}\n`
 }
 
 const render = articles => {
