2102da0e21a7bc7142046ab3c381668164d6ac39
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2019-10-17 15:30:05 +0200

    to be precise..

--- jest.ci.js
+++ jest.ci.js
@@ -3,7 +3,7 @@ module.exports = {
   ...defaultJestConfig,
   testMatch: ['**/test/**/?(*.)+(spec|test).js?(x)'],
   collectCoverage: true,
-  //TODO: shame...
+  //TODO: super-shame...
   coverageThreshold: {
     global: {branches: 30, functions: 30, lines: 30, statements: 30}
   },
