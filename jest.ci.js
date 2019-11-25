28947180cb40cebdb7bae10fa4643c38dd506bb4
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2019-11-25 19:36:10 +0100

    ignoring a lint songarjs issue.. the status of lint and tests is so low now that is useless to try to randomly improve, need a serious re-shape...

--- jest.ci.js
+++ jest.ci.js
@@ -2,7 +2,7 @@ const defaultJestConfig = require('./jest.config')
 module.exports = {
   ...defaultJestConfig,
   collectCoverage: true,
-  //TODO: super-shame...
+  // TODO: super-shame...
   coverageThreshold: {
     global: {branches: 30, functions: 30, lines: 30, statements: 30}
   },
