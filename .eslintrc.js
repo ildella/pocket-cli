21f4a474cb832a319b035472eea257b85ce79a9e
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2020-06-16 17:12:40 +0200

    using eslintrc in js, not yaml

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: [
+    'node-opinionated',
+  ],
+  overrides: [
+    {
+      files: ['**/*test*/**', '**/fixtures/**', '*test.*'],
+      rules: {
+        'node/no-unpublished-require': 'off',
+        'node/no-unpublished-import': 'off',
+        'max-nested-callbacks': ['warn', 3],
+        'max-lines': ['warn', 200],
+        'sonarjs/no-duplicate-string': 'off',
+        'no-console': 'off',
+        'no-sync': 'off',
+        'no-undefined': 'off',
+        'security/detect-child-process': 'off',
+        'security/detect-non-literal-fs-filename': 'off',
+        'security/detect-non-literal-require': 'off'
+      }
+    },
+  ]
+}
